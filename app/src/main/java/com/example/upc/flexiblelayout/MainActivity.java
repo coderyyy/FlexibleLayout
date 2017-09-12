973a68fc2e48165438479b5b02eb48d66942def4
coderyyy <zy315773787@gmail.com>
2017-09-12 22:34:32 +0800

    added - TextView background & click listener

--- app/src/main/java/com/example/upc/flexiblelayout/MainActivity.java
+++ app/src/main/java/com/example/upc/flexiblelayout/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.upc.flexiblelayout;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.flexible.FlexibleLayout;
 
@@ -23,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         final LinearLayout topLy = (LinearLayout) findViewById(R.id.toggle_top);
         final RelativeLayout midLy = (RelativeLayout) findViewById(R.id.toggle_mid);
         final RelativeLayout bottomLy = (RelativeLayout) findViewById(R.id.toggle_bottom);
+        final TextView topTv = (TextView) findViewById(R.id.toggle_top_tv);
+        final TextView midTv = (TextView) findViewById(R.id.toggle_mid_tv);
+        final TextView bottomTv = (TextView) findViewById(R.id.toggle_bottom_tv);
 
         flexibleLayout = (FlexibleLayout) findViewById(R.id.toggle);
 
@@ -37,6 +43,27 @@ public class MainActivity extends AppCompatActivity {
                 flexibleLayout.setToClose();
                 flexibleLayout.getBackground().setAlpha(0);// 设置为透明色
                 flexibleLayout.setOnScrollListener(mOnScrollListener);
+            }
+        });
+
+        topTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, topTv.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        midTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, midTv.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, bottomTv.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
